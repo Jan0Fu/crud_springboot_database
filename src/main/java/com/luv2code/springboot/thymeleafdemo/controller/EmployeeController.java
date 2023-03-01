b703feebccf72dd053080784014bac531615e273
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2023-03-01 10:19:42 +0100

    feat: add repository with datasource connection

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,35 +1,25 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
-import com.luv2code.springboot.thymeleafdemo.model.Employee;
-import jakarta.annotation.PostConstruct;
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    private List<Employee> theEmployees;
-
-    @PostConstruct
-    private void loadData() {
-
-        Employee empl1 = new Employee(1, "Leslie", "Andrews", "leslie@and.com");
-        Employee empl2 = new Employee(2, "Jane", "Doe", "jane@doe.com");
-        Employee empl3 = new Employee(3, "John", "Smith", "john@smt.com");
-        theEmployees = new ArrayList<>();
-        theEmployees.add(empl1);
-        theEmployees.add(empl2);
-        theEmployees.add(empl3);
-    }
+    @Autowired
+    private EmployeeService employeeService;
 
     @GetMapping("/list")
     public String ss(Model theModel) {
+        List<Employee> theEmployees = employeeService.findAll();
         theModel.addAttribute("employees", theEmployees);
         return "list-employees";
     }
--- src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
@@ -20,7 +20,8 @@ public class Employee {
     public Employee() {
     }
 
-    public Employee(String firstName, String lastName, String email) {
+    public Employee(int id, String firstName, String lastName, String email) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
