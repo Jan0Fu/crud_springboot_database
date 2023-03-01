e0b6fb7971a3ba133587e7bcce0115b6552b0f14
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2023-03-01 09:20:19 +0100

    refactor: reuse model from previous project

--- src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
@@ -1,16 +1,26 @@
-package com.luv2code.springboot.thymeleafdemo.model;
+package com.luv2code.springboot.thymeleafdemo.entity;
 
+import jakarta.persistence.*;
+
+@jakarta.persistence.Entity
+@Table(name="employee")
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private int id;
+    @Column(name="first_name")
     private String firstName;
+    @Column(name="last_name")
     private String lastName;
+    @Column(name="email")
     private String email;
 
-    public Employee() {}
+    public Employee() {
+    }
 
-    public Employee(int id, String firstName, String lastName, String email) {
-        this.id = id;
+    public Employee(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
